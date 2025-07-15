ac802b26c883d5137f6e12d0998398cbf4e7dd67
pkonsomu2020 <pkonsomu2021@gmail.com>
2025-07-15 20:51:41 +0300

    Updated K-GROUP

--- src/pages/LinksAutoMotors.tsx
+++ src/pages/LinksAutoMotors.tsx
@@ -21,7 +21,7 @@ const LinksAutoMotors = () => (
     </ThemeProvider>
     {/* Hero Banner */}
     <section className="relative flex flex-col items-center justify-center text-center px-4 pt-8 pb-12 bg-brand-black overflow-hidden">
-      <img src="/src/assets/dj-kach-hero.jpg" alt="Banner Background" className="absolute inset-0 w-full h-full object-cover opacity-30 z-0" />
+      <img src="/assets/dj-kach-hero.jpg" alt="Banner Background" className="absolute inset-0 w-full h-full object-cover opacity-30 z-0" />
       <div className="absolute inset-0 bg-black/40 z-0" />
       <div className="relative z-10 flex flex-col items-center w-full">
         <div className="w-48 h-48 rounded-full bg-white border-4 border-brand-red flex items-center justify-center mx-auto mb-2 overflow-hidden">
