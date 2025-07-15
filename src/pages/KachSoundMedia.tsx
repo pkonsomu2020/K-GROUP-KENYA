4eba1d222e7b8b81dace68048d4f6e8c23f226ec
pkonsomu2020 <pkonsomu2021@gmail.com>
2025-07-15 20:55:13 +0300

    Updated K-GROUP

--- src/pages/KachSoundMedia.tsx
+++ src/pages/KachSoundMedia.tsx
@@ -22,7 +22,7 @@ const KachSoundMedia = () => (
     {/* Hero Banner - match screenshot */}
     <section className="relative flex flex-col items-center justify-center text-center px-4 pt-8 pb-12 bg-brand-black overflow-hidden">
       {/* Background image with lighter overlay */}
-      <img src="/src/assets/dj-kach-hero.jpg" alt="Banner Background" className="absolute inset-0 w-full h-full object-cover opacity-30 z-0" />
+      <img src="/assets/dj-kach-hero.jpg" alt="Banner Background" className="absolute inset-0 w-full h-full object-cover opacity-30 z-0" />
       <div className="absolute inset-0 bg-black/40 z-0" />
       <div className="relative z-10 flex flex-col items-center w-full">
         <div className="w-48 h-48 rounded-full bg-white border-4 border-brand-red flex items-center justify-center mx-auto mb-2 overflow-hidden">
