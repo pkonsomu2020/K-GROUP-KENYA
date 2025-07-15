f57c074c50c3af67fbf8e5dc24e667622fdae5e5
pkonsomu2020 <pkonsomu2021@gmail.com>
2025-07-16 00:04:02 +0300

    Updated K-GROUP

--- src/pages/KBRAcademy.tsx
+++ src/pages/KBRAcademy.tsx
@@ -22,7 +22,7 @@ const KBRAcademy = () => (
     </ThemeProvider>
     {/* Hero Banner */}
     <section className="relative flex flex-col items-center justify-center text-center px-4 pt-24 pb-12 bg-brand-black overflow-hidden">
-      <img src="/src/assets/dj-kach-hero.jpg" alt="Banner Background" className="absolute inset-0 w-full h-full object-cover opacity-30 z-0" />
+      <img src="/dj-kach-hero.jpg" alt="Banner Background" className="absolute inset-0 w-full h-full object-cover opacity-30 z-0" />
       <div className="absolute inset-0 bg-black/40 z-0" />
       <div className="relative z-10 flex flex-col items-center w-full">
         <div className="relative w-40 h-40 rounded-full bg-white flex items-center justify-center shadow-lg border-4 border-brand-red mt-0 mb-4 overflow-hidden">
